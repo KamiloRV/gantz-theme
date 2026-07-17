62780ec46a2a6f5eab905855d4009743ea8c17a8
Paola Véliz <paola.veliz@gmail.com>
2026-07-16 23:10:04 -0400

    Validar rut en functions.php

--- functions.php
+++ functions.php
@@ -47,3 +47,79 @@ require_once get_template_directory() . '/inc/newsletter/install.php';
 require_once get_template_directory() . '/inc/newsletter/admin.php';
 require_once get_template_directory() . '/inc/newsletter/ajax.php';
 require_once get_template_directory() . '/inc/newsletter/cf7.php';
+
+/**
+ * Validación de RUT Chileno para Contact Form 7
+ */
+
+add_filter('wpcf7_validate_text*', 'validar_rut_cf7', 20, 2);
+add_filter('wpcf7_validate_text', 'validar_rut_cf7', 20, 2);
+
+function validar_rut_cf7($result, $tag) {
+
+    $tag = new WPCF7_FormTag($tag);
+
+    // Campos que deben validarse como RUT
+    $campos_rut = array(
+        'rut-paciente',
+        'rut-cuidador'
+    );
+
+    // Si el campo no está en la lista, no hacer nada
+    if (!in_array($tag->name, $campos_rut)) {
+        return $result;
+    }
+
+    $rut = isset($_POST[$tag->name]) ? trim($_POST[$tag->name]) : '';
+
+    if (!validar_rut_chileno($rut)) {
+        $result->invalidate(
+            $tag,
+            'Ingrese un RUT válido. Ejemplo: 12.345.678-5.'
+        );
+    }
+
+    return $result;
+}
+
+function validar_rut_chileno($rut) {
+
+    $rut = strtoupper($rut);
+    $rut = preg_replace('/[^0-9K]/', '', $rut);
+
+    if (strlen($rut) < 2) {
+        return false;
+    }
+
+    $dv = substr($rut, -1);
+    $numero = substr($rut, 0, -1);
+
+    $suma = 0;
+    $multiplo = 2;
+
+    for ($i = strlen($numero) - 1; $i >= 0; $i--) {
+
+        $suma += intval($numero[$i]) * $multiplo;
+
+        $multiplo++;
+
+        if ($multiplo > 7) {
+            $multiplo = 2;
+        }
+    }
+
+    $dvCalculado = 11 - ($suma % 11);
+
+    switch ($dvCalculado) {
+        case 11:
+            $dvCalculado = '0';
+            break;
+        case 10:
+            $dvCalculado = 'K';
+            break;
+        default:
+            $dvCalculado = (string)$dvCalculado;
+    }
+
+    return $dv === $dvCalculado;
+}
